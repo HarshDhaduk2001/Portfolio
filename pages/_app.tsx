75cd998e0ffe088811f85599cbfd97f52e7f6c4e
HarshDhaduk2001 <harshdhaduk2001@gmail.com>
2024-01-10 20:46:37 +0530

    portfolio created without project added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,40 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import AppContext from "../components/AppContextFolder/AppContext";
+import { useRef, useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: any) {
+  const timerCookie = useRef(null);
+  const windowSizeTrackerRef = useRef(null);
+  const mousePositionRef = useRef(null);
+  const [sharedState, setSharedState] = useState({
+    portfolio: {
+      NavBar: {
+        IntervalEvent: null,
+        scrolling: null,
+        scrollSizeY: null,
+      },
+      Scrolling: {
+        IntervalEvent: null,
+      },
+    },
+    userdata: {
+      timerCookieRef: timerCookie,
+      windowSizeTracker: windowSizeTrackerRef,
+      mousePositionTracker: mousePositionRef,
+    },
+    typing: {
+      keyboardEvent: null,
+      eventInputLostFocus: null,
+    },
+    finishedLoading: false,
+  });
+  return (
+    <AppContext.Provider value={{ sharedState, setSharedState }}>
+      <Component {...pageProps} />
+      <Analytics />
+    </AppContext.Provider>
+  );
 }
+
+export default MyApp;
