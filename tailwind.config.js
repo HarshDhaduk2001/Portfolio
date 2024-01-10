b7cb99ddb9692df58bd4be69655f6a893f53d763
HarshDhaduk2001 <harshdhaduk2001@gmail.com>
2024-01-10 21:02:29 +0530

    updated to portfolio_latest_next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,70 +1,60 @@
 /** @type {import('tailwindcss').Config} */
-
-const { fontFamily } = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
   theme: {
+    rotate: {
+      "-180": "-180deg",
+      "-90": "-90deg",
+      "-45": "-45deg",
+      0: "0",
+      45: "45deg",
+      90: "90deg",
+      135: "135deg",
+      180: "180deg",
+      270: "270deg",
+    },
     extend: {
       fontFamily: {
-        mont: ["var(--font-mont)", ...fontFamily.sans],
+        Header: ["Lato", "sans-serif"],
+        Text2: ["Lato", "sans-serif"],
+        About: ["Playfair Display", "serif"],
+        Arimo: ["Arimo", "sans-serif"],
       },
       colors: {
-        dark: "#1b1b1b",
-        light: "#f5f5f5",
-        primary: "#B63E96", // 240,86,199
-        primaryDark: "#58E6D9", // 80,230,217
+        AAprimary: "#0b192f",
+        AAsecondary: "#64ffda",
+        AAError: "#ff6489",
+        AAtertiary: "#112340",
+        ResumeButtonHover: "#153040",
+        MobileNavBarColor: "#112340",
+        StartupBackground: "#020c1b",
       },
-      backgroundImage: {
-        circularLight:
-            "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px)",
-    
-        circularDark:
-            "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 100px)",
-    
-        circularLightLg:
-            "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 80px)",
-    
-        circularDarkLg:
-            "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 80px)",
-    
-        circularLightMd:
-            "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px)",
-    
-        circularDarkMd:
-            "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 6px,#1b1b1b 60px)",
-    
-        circularLightSm:
-            "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px)",
-    
-        circularDarkSm:
-            "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 4px,#1b1b1b 40px)",
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "5xl": "3840px",
       },
-    },
-    screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
-      md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
-
-      xs: { max: "479px" },
-      // => @media (max-width: 479px) { ... }
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
+  variants: {
+    scrollbar: ["rounded"],
+  },
 };
