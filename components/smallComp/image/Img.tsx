c3716a7b0e24de906b01a8b53d83ef96dfd62a17
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-06-01 08:29:56 +0530

    react portfolio

--- components/smallComp/image/Img.tsx
+++
@@ -1,6 +0,0 @@
-export default function Img(props: any) {
-  return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img src={props.src} alt={props.alt} className={props.className} />
-  );
-}
---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1.5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1.5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Executive Full Stack Developer",
+    company: "Technomark",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB or SQL databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: [
+      "Javascript",
+      "React.js",
+      "Next.js",
+      "NodeJs",
+      "mongoDB",
+      "SQL",
+      "TailwindCSS",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "PROJECT MANAGEMENT SYSTEM",
+    image: project1,
+    description: `In this project I have managed and led a cross-functional team to deliver a
+      Next.js-based frontend project in 4 months. It has all the functionalities how
+      an organization works, all the projects and tasks management was done from
+      one single website. Ii’s a website where you can manage your clients, users
+      and their task with the given time and charge accordingly.`,
+    technologies: ["NextJS", "TailwindCSS", "TypeScript", ".Net"],
+  },
+  {
+    title: "NPM PACKAGES",
+    image: project2,
+    description: `In this we have created npm packages to use it as a part of ui with all the
+      functionalities was added inside package we can also use it as per our
+      requirements we can add and remove some functionalities.`,
+    technologies: ["HTML", "CSS", "JavaScript", "React"],
+  },
+  {
+    title: "MEMORIES APP",
+    image: project3,
+    description: `Based on React.js, Node .js, Express.js and MongoDB.
+      The Memories App is a MERN stack web application that allows users to
+      upload, store, and share their memories. It features a user-friendly interface
+      with functionalities such as user authentication, search, and filtering, and
+      social media sharing. The back-end is built with Node.js and MongoDB, while
+      the front-end is built with React.`,
+    technologies: ["HTML", "CSS", "React", "Node", "Express"],
+  },
+  {
+    title: "BLOGGING PLATFORM",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "React", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Ahmedabad 382345",
+  phoneNo: "",
+  email: "harshdhaduk2001@gmail.com",
+};
